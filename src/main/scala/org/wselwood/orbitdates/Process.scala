97daa56a98fb9f521b596fc63db8e122c69de097
Wil Selwood <wselwood@gmail.com>
2017-11-04 07:22:38 +0000

    unpack ids and list numbers

--- src/main/scala/org/wselwood/orbitdates/Process.scala
+++ src/main/scala/org/wselwood/orbitdates/Process.scala
@@ -4,6 +4,7 @@ import java.time.format.DateTimeFormatter
 import java.time.temporal.ChronoUnit
 import java.time.{LocalDate, LocalDateTime, ZoneOffset}
 
+import org.apache.spark.SparkContext
 import org.apache.spark.sql._
 import org.apache.spark.sql.catalyst.expressions.Substring
 import org.apache.spark.sql.functions._
@@ -17,38 +18,55 @@ object Process {
       return
     }
 
-
     val observationPath = args(0)
     val mpcobs = args(1)
 
     val spark = SparkSession
       .builder()
-      .master("local[2]")
+      .master("local[2]") // if you have more cores or a cluster turn this up.
       .appName("Spark SQL join observations")
       .getOrCreate()
 
+    spark.sparkContext.setCheckpointDir("/tmp/")
+
     val dateConvert = udf(dateFunc _)
-    val trimZero = udf(trimZeroFunc _)
+    val unpackId = udf(unpackIdFunc _)
     val formatDate = udf(formatDateFunc _)
 
     val obs = spark.read.text(observationPath)
-      .withColumn("id", trimZero(substring(col("value"), 0, 5)))
+      .withColumn("id", unpackId(substring(col("value"), 0, 5)))
       .withColumn("ts", dateConvert(substring(col("value"), 16, 16)))
       .groupBy(col("id"))
       .min("ts")
+      .checkpoint()
 
     val orbRec = spark.read
       .option("multiLine", true)
       .json(mpcobs)
       .withColumn("id", new Column(Substring(col("Number").expr, lit(2).expr, (length(col("Number"))-2).expr)))
+      .checkpoint()
 
     val result = obs.join(orbRec, "id")
+      .select(
+        col("id"),
+        col("Name"),
+        formatDate(col("min(ts)")).as("FirstObs"),
+        col("a"),
+        col("e"),
+        col("i"),
+        col("Epoch"),
+        col("H"),
+        col("G"),
+        col("Node"),
+        col("Peri")
+      )
+      .checkpoint()
 
     //id,Name,FirstObs,a,e,i,Epoch,H,G,Node,Peri
 
-    result.select(col("id"), col("Name"), formatDate(col("min(ts)")).as("FirstObs"),
-      col("a"), col("e"), col("i"), col("Epoch"), col("H"), col("G"), col("Node"), col("Peri"))
-      .write.mode(SaveMode.Overwrite).csv("out.csv")
+    result.write.mode(SaveMode.Overwrite).csv("out.csv")
+
+    println("obs objects: " + obs.count() + " objects: " + orbRec.count() + " result: " + result.count())
 
   }
 
@@ -72,6 +90,21 @@ object Process {
       i = i + 1
     }
     in.substring(i)
+  }
+
+  private def unpackIdFunc(in : String) : String = {
+    if (in(0) >= '0' && in(0) <= '9') {
+      trimZeroFunc(in)
+    } else {
+      val numeric = in.substring(1).toInt
+      val expanded = if (in(0) >= 'a' && in(0) <= 'z') {
+        in(0) - 'a' + 36
+      } else {
+        in(0) - 'A' + 10
+      }
+
+      expanded.toString + numeric.toString
+    }
   }
 
   private def formatDateFunc(in : Long) : String = {
